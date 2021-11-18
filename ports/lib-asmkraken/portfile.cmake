443805528046476d41469258f9de156c8c0af9bd
AridTag <AridTag@binarydepths.com>
2021-11-18 00:35:37 -0500

    This ones gonna work..

--- ports/lib-asmkraken/portfile.cmake
+++ ports/lib-asmkraken/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO AridTag/lib-asmkraken
-        REF a55dbe2
-        SHA512 a0650348cb37040bae601d1a1f45c1610abe3c0bc029a55e1e1fee37700e70c0e3e8fbc2d55f914ffaa50322bd1d08e15393a83a34df2bfe4ed962b497640126
+        REF 67159d15947c547728745db6b44fae73f461b00a
+        SHA512 2a50aebad61eaa02cd6d91386857698e0f5a8c7e21c6f37043e536b0288106e6b6c25471da805c74eb8226c1406a3c27e220e2610993fb5ae6bda6200e2ea837
         HEAD_REF master
 )
 
@@ -11,5 +11,7 @@ vcpkg_configure_cmake(
         PREFER_NINJA
 )
 vcpkg_install_cmake()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 file(INSTALL ${SOURCE_PATH}/LICENSE.md DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
