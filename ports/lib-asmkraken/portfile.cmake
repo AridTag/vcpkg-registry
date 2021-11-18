0fa72bee85168b6fb3a7b7fc80d8855c37e9a166
AridTag <AridTag@binarydepths.com>
2021-11-17 23:06:04 -0500

    Add missing baseline

--- ports/lib-asmkraken/portfile.cmake
+++ ports/lib-asmkraken/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO AridTag/lib-asmkraken
         REF a55dbe2
-        SHA512 0
+        SHA512 a0650348cb37040bae601d1a1f45c1610abe3c0bc029a55e1e1fee37700e70c0e3e8fbc2d55f914ffaa50322bd1d08e15393a83a34df2bfe4ed962b497640126
         HEAD_REF master
 )
 
